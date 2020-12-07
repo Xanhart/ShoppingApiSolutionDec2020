ebab345336fbb61854ad2aa564e156d399e4abfd
Xander Barnhart <31421849+Xanhart@users.noreply.github.com>
2020-12-07 14:40:46 -0500

    Add project files.

---
+++ ShoppingApi/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShoppingApi.Models;
+using ShoppingApi.Models.Products;
+using ShoppingApi.Services;
+
+namespace ShoppingApi.Controllers
+{
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILookupProducts _productLookup;
+
+        public ProductsController(ILookupProducts productLookup)
+        {
+            _productLookup = productLookup;
+        }
+
+        [HttpGet("/products/{id:int}")]
+        public async Task<ActionResult> GetProductsById(int Id)
+        {
+            GetProductDetailsResponse response = await _productLookup.GetProductById(Id);
+            return Ok(response);
+        }
+    }
+}
---
+++ ShoppingApi/Services/EfSqlProducts.cs
@@ -0,0 +1,23 @@
+﻿using ShoppingApi.Models.Products;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingApi.Services
+{
+    public class EfSqlProducts : ILookupProducts
+    {
+        public Task<GetProductDetailsResponse> GetProductById(int id)
+        {
+            return Task.FromResult(new GetProductDetailsResponse // LOL what is Task.FromResult / im sure you know just stop being a j'babby
+            {
+                Id = id,
+                Name = "Tofu or something like that IDK maybe Carrots? Seaweed? CORN?",
+                Category = "Vegan",
+                Count = 1,
+                Price = 4.99M
+            });
+        }
+    }
+}
